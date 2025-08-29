8dbf3d32c04ab24f66462b63e59a95dd8b4d4a7b
Amrou Hichem Zorgani <164809141+zorganiamrouhichem@users.noreply.github.com>
2025-08-29 18:45:26 +0100

    Add ImageGallery component to portfolio page

--- src/app/(portfolio)/page.tsx
+++ src/app/(portfolio)/page.tsx
@@ -1,4 +1,5 @@
 import Hero from "../components/Hero";
+import ImageGallery from "../components/ImageGallery";
 import Navbar from "../components/Navbar";
 import Tech from "../components/Tech";
 import Technologies from "../components/Technologies";
@@ -10,6 +11,7 @@ export default function Home() {
       <Hero/>
       <Technologies/>
       <Tech/>
+      <ImageGallery/>
     </>
   );
 }
